4ad0259ad1f43912e639131cdced848df7d6b177
shiboshreeroy <shiboshreeroy169@gmail.com>
2025-06-06 18:30:29 +0600

    Update Teen

--- src/components/Home/Resources/Categories/Teen.jsx
+++ src/components/Home/Resources/Categories/Teen.jsx
@@ -28,7 +28,7 @@ export default function Teen() {
 
   return (
     <section className="bg-black text-white px-6 py-10 min-h-screen">
-      <h2 className="text-3xl font-bold mb-6 text-center">🔥 Featured Ten Premium Videos</h2>
+      <h2 className="text-3xl font-bold mb-6 text-center">🔥 Featured Teen Premium Videos</h2>
 
       {/* Video Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
